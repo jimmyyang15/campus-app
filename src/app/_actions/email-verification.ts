da7df7f9e2a87abed1ffe72b21845db8684d812f
andriankreasitech <huangandrian02@gmail.com>
2024-06-19 13:04:49 +0700

    test push

--- src/app/_actions/email-verification.ts
+++ src/app/_actions/email-verification.ts
@@ -66,7 +66,7 @@ export async function generateRedirectUrl(props: {
         }
     );
 
-    const redirectUrl = `${env.NODE_ENV === "development" ? "http://localhost:3000" : "https://campus-app-one.vercel.app/"}/auth/verify-email?token=${token}`;
+    const redirectUrl = `${env.NODE_ENV === "development" ? "http://localhost:3000" : "https://campus-app-one.vercel.app"}/auth/verify-email?token=${token}`;
 
     return redirectUrl
 }
