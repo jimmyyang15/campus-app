6d2f378bccdeb2b17ef069ad4cb3db1f6d885db1
andriankreasitech <huangandrian02@gmail.com>
2024-06-26 14:44:44 +0700

    add user nims

--- src/user-nim.ts
+++ src/user-nim.ts
@@ -2,11 +2,14 @@ export const userNim = [
  '2044091',
  '2044094',
  '2044090',
+ "2044093",
+ "2044089",
  "2044088"
 ];
 
 export const mentorNid = [
-    '1234567'
+    '1234567',
+    "1122334"
 ];
 
 export const adminUsername = [
